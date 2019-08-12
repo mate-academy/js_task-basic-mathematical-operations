9327ea7496eebe6486a59b85369d497ddc8f189f
amserhii11 <amelin.serhii@gmail.com>
2019-08-12 20:06:19 +0300

    Fixed 2

--- src/basicOp.js
+++ src/basicOp.js
@@ -33,8 +33,9 @@ function basicOp(operation, value1, value2) {
       return value1 * value2;
     case '/':
       return value1 / value2;
+    default:
+      return 0;
   }
-  return 0;
 }
 
 module.exports = basicOp;
