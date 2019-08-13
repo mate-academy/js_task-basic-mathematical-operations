e006221ad287f8e41028b1dbd556010889f37eaf
vladyslav223 <vladyslav223@gmail.com>
2019-08-13 16:32:08 +0300

    switched

--- src/basicOp.js
+++ src/basicOp.js
@@ -24,20 +24,18 @@
  * @return {number}
  */
 function basicOp(operation, value1, value2) {
-  // write code here
-  let result;
-  if (operation === '+') {
-    result = value1 + value2;
-  } else if (operation === '-') {
-    result = value1 - value2;
-  } else if (operation === '*') {
-    result = value1 * value2;
-  } else if (operation === '/') {
-    if (value1 && value2) {
-      result = value1 / value2;
-    }
-  } else { return 0; }
-  return result;
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
+  }
 }
 
 module.exports = basicOp;
