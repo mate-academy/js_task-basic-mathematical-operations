c0568244485216a1cd53958362d43780753dad26
Vladimir <koretskyi@gmail.com>
2019-08-13 13:02:33 +0300

    fixed

--- src/basicOp.js
+++ src/basicOp.js
@@ -24,16 +24,18 @@
  * @return {number}
  */
 function basicOp(operation, value1, value2) {
-  if (operation === '+') {
-    return value1 + value2;
-  } else if (operation === '-') {
-    return value1 - value2;
-  } else if (operation === '*') {
-    return value1 * value2;
-  } else if (operation === '/') {
-    return value1 / value2;
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
   }
-  return 0;
 }
 
 module.exports = basicOp;
