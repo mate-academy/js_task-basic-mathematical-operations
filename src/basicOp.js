773f21e90534269f44166c1342596e5d34c51595
Anna Kazantseva <annetastara@gmail.com>
2019-09-23 15:27:03 +0300

    Created default

--- src/basicOp.js
+++ src/basicOp.js
@@ -44,7 +44,9 @@ function basicOp(operation, value1, value2) {
       return value1 * value2;
     case '/':
       return value1 / value2;
+    default:
+      return 0;
   }
-  return 0;
 }
+
 module.exports = basicOp;
