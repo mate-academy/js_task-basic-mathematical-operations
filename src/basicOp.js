f3a157fd0d7fd62ef9781607f2703161ee651be9
VitaliyStarodub <starodub.vital@gmail.com>
2019-09-29 21:41:58 +0300

    Solution

--- src/basicOp.js
+++ src/basicOp.js
@@ -25,6 +25,20 @@
  */
 function basicOp(operation, value1, value2) {
   // write code here
+  let result;
+  switch (operation) {
+    case '+': result = value1 + value2;
+      break;
+    case '-': result = value1 - value2;
+      break;
+    case '*': result = value1 * value2;
+      break;
+    case '/': result = value1 / value2;
+      break;
+    default : result = 0;
+  }
+
+  return result;
 }
 
 module.exports = basicOp;
