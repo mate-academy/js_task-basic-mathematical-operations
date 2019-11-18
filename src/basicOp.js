e31c26ccd11cc5534af4b1a29284205d3a4270dd
Vivych Oksana <vivichokcana@gmail.com>
2019-11-18 17:41:13 +0200

    'Solution'

--- src/basicOp.js
+++ src/basicOp.js
@@ -26,13 +26,17 @@
 function basicOp(operation, value1, value2) {
   if (operation === '+') {
     return value1 + value2;
-  } else if (operation === '-') {
+  }
+  if (operation === '-') {
     return value1 - value2;
-  } else if (operation === '*') {
+  }
+  if (operation === '*') {
     return value1 * value2;
-  } else if (operation === '/') {
+  }
+  if (operation === '/') {
     return value1 / value2;
-  } return 0;
+  }
+  return 0;
 }
 
 module.exports = basicOp;
