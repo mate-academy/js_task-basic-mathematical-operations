8a7aaf304d00fbfb71f3320f55357f3724345f90
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-20 13:30:14 +0200

    solution

--- src/basicOp.js
+++ src/basicOp.js
@@ -24,7 +24,11 @@
  * @return {number}
  */
 function basicOp(operation, value1, value2) {
-  // write code here
+  return (operation === '+') ? value1 + value2
+    : (operation === '-') ? value1 - value2
+      : (operation === '*') ? value1 * value2
+        : (operation === '/') ? value1 / value2
+          : 0;
 }
 
 module.exports = basicOp;
