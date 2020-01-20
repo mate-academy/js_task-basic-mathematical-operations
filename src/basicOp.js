a0bf204e100de52bde533ab35246fc8169bda157
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-20 15:49:26 +0200

    add switch

--- src/basicOp.js
+++ src/basicOp.js
@@ -24,17 +24,27 @@
  * @return {number}
  */
 function basicOp(operation, value1, value2) {
-  if (operation === '+') {
-    return value1 + value2;
-  } else if (operation === '-') {
-    return value1 - value2;
-  } else if (operation === '*') {
-    return value1 * value2;
-  } else if (operation === '/') {
-    return value1 / value2;
-  }
+  switch (operation) {
+    case '+': {
+      return value1 + value2;
+    }
 
-  return 0;
+    case '-': {
+      return value1 - value2;
+    }
+
+    case '*': {
+      return value1 * value2;
+    }
+
+    case '/': {
+      return value1 / value2;
+    }
+
+    default: {
+      return 0;
+    }
+  }
 }
 
 module.exports = basicOp;
