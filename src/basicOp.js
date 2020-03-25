ca1c6e4519386dd240007f7b873eda4819cf5611
kate <kate@test.com>
2020-03-25 13:05:49 +0200

    solution

--- src/basicOp.js
+++ src/basicOp.js
@@ -25,6 +25,23 @@
  */
 function basicOp(operation, value1, value2) {
   // write code here
+  if (operation === '+') {
+    return value1 + value2;
+  }
+
+  if (operation === '-') {
+    return value1 - value2;
+  }
+
+  if (operation === '*') {
+    return value1 * value2;
+  }
+
+  if (operation === '/') {
+    return value1 / value2;
+  }
+
+  return 0;
 }
 
 module.exports = basicOp;
