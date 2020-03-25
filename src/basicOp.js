934114150355e65b13ac4b6301334e1be75ef863
Andrey Faychak <faychak333@gmail.com>
2020-03-25 12:38:29 +0200

    'solution'

--- src/basicOp.js
+++ src/basicOp.js
@@ -25,6 +25,23 @@
  */
 function basicOp(operation, value1, value2) {
   // write code here
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+      break;
+    case '-':
+      return value1 - value2;
+      break;
+    case '*':
+      return value1 * value2;
+      break;
+    case '/':
+      return value1 / value2;
+      break;
+    default:
+      return 0;
+      break;
+  }
 }
 
 module.exports = basicOp;
