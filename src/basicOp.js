50610a4375a79587ae7590c31468b2edeed37a02
euxos <eugen.ostrozhinsky@gmail.com>
2020-03-26 06:29:20 +0200

    solution

--- src/basicOp.js
+++ src/basicOp.js
@@ -28,12 +28,20 @@ function basicOp(operation, value1, value2) {
 
   if (operation === '+') {
     result = value1 + value2;
+
+    return result;
   } else if (operation === '-') {
     result = value1 - value2;
+
+    return result;
   } else if (operation === '*') {
     result = value1 * value2;
+
+    return result;
   } else if (operation === '/') {
     result = value1 / value2;
+
+    return result;
   }
 
   return result;
