7dfe26eee7d6abc523d1c920b169ae0475e584ff
DimaDP <dimadp95@gmail.com>
2020-05-20 11:26:31 +0300

    dimadp_solution

--- src/basicOp.js
+++ src/basicOp.js
@@ -1,30 +1,14 @@
 'use strict';
 
-/**
- * Your task is to create a function that does four basic mathematical
- * operations.
- *
- * Implement basicOp function:
- *
- * The function should return result of numbers after applying
- * the chosen operation.
- *
- * Examples:
- *
- * basicOp('+', 4, 7)                     // return 11
- * basicOp('-', 15, 18)                   // return -3
- * basicOp('*', 5, 5)                     // return 25
- * basicOp('/', 49, 7)                    // return 7
- * basicOp('something else', 45, 45)      // return 0
- *
- * @param {string} operation
- * @param {number} value1
- * @param {number} value2
- *
- * @return {number}
- */
 function basicOp(operation, value1, value2) {
-  // write code here
+  const operators = {
+    '+': value1 + value2,
+    '-': value1 - value2,
+    '*': value1 * value2,
+    '/': value1 / value2,
+  };
+
+  return (operators[operation]) ? operators[operation] : 0;
 }
 
 module.exports = basicOp;
